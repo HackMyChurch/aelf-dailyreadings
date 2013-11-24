d91b118cc0dabf8a5e2ae5d434fa9649cc734dcd
Jean-Tiare LE BIGOT <admin@jtlebi.fr>
2013-11-24 21:37:45 +0100

    rendering improvments: add &nbsp; where needed + enable pinch zoom

--- src/co/epitre/aelf_lectures/LectureFragment.java
+++ src/co/epitre/aelf_lectures/LectureFragment.java
@@ -27,19 +27,27 @@ public class LectureFragment extends Fragment {
 		
 		// compute view --> HTML
 		StringBuilder htmlString = new StringBuilder();
+		String body = getArguments().getString(ARG_DESCRIPTION)
+				.replace(" :", "&nbsp;:")
+				.replace(" !", "&nbsp;!")
+				.replace(" ?", "&nbsp;?")
+				.replace(" &raquo;", "&nbsp;&raquo;")
+				.replace("&laquo; ", "&laquo;&nbsp;");
 		htmlString.append("<html><head><style type=\"text/css\">body{margin:24px}</style></head><body>");
 		htmlString.append("<h3>" + getArguments().getString(ARG_LONG_TITLE) + "</h3>");
-		htmlString.append(getArguments().getString(ARG_DESCRIPTION));
+		htmlString.append(body);
 		htmlString.append("</body></html>");
 
 		// actual UI refresh
 		View rootView = inflater.inflate(R.layout.fragment_lecture, container, false);
 		WebView lectureView = (WebView) rootView.findViewById(R.id.LectureView);
+		lectureView.getSettings().setBuiltInZoomControls(true);
 		lectureView.loadDataWithBaseURL("file:///android_asset/", htmlString.toString(), "text/html", "utf-8", null);
 		lectureView.setBackgroundColor(0x00000000);
 		if(android.os.Build.VERSION.SDK_INT > 11)
 		{
 			lectureView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+			lectureView.getSettings().setDisplayZoomControls(false);
 		}
 		// return
 		return rootView;
