d3a904ab08b34d15773db2c870d7c5e90da31a7a
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2015-12-20 20:32:48 +0100

    display preferences in fullscreen to avoid flicker

--- app/src/main/java/co/epitre/aelf_lectures/SyncPrefActivity.java
+++ app/src/main/java/co/epitre/aelf_lectures/SyncPrefActivity.java
@@ -1,10 +1,13 @@
 package co.epitre.aelf_lectures;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 public class SyncPrefActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
     public static final String KEY_PREF_DISP_FONT_SIZE = "pref_disp_font_size";
@@ -57,6 +60,39 @@ public class SyncPrefActivity extends PreferenceActivity implements OnSharedPref
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.commit(); // commit to file so that sync service is able to load it from disk
         }
+    }
+
+    @SuppressLint("NewApi")
+    public void prepare_fullscreen() {
+        // copied from LectureActivity
+        // Hide status (top) bar. Navigation bar (> 4.0) still visible.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (android.os.Build.VERSION.SDK_INT < 14) // 4.0 min
+            return;
+
+        // Android 4.0+: make navigation bar 'discret' ('dots' instead of icons)
+        int uiOptions = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+
+        // Android 4.4+: hide navigation bar, make it accessible via edge scroll
+        if (android.os.Build.VERSION.SDK_INT >= 19) {
+            uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    |  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
+
+        // apply settings
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // manage application's intrusiveness for different Android versions
+        super.onWindowFocusChanged(hasFocus);
+
+        if(hasFocus)
+            prepare_fullscreen();
     }
 
 }
