22f4ef9cbf0f159f29995013ee1b7c76461584af
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2016-01-10 12:32:08 +0100

    bump version to 1.5.0

--- app/src/main/java/co/epitre/aelf_lectures/LecturesActivity.java
+++ app/src/main/java/co/epitre/aelf_lectures/LecturesActivity.java
@@ -112,7 +112,7 @@ public class LecturesActivity extends ActionBarActivity implements DatePickerFra
 
         // upgrade logic, primitive at the moment...
         if(savedVersion != currentVersion) {
-            if(savedVersion < 14) {
+            if(savedVersion < 15) {
                 // delete cache DB: needs to force regenerate
                 getApplicationContext().deleteDatabase("aelf_cache.db");
                 // regenerate, according to user settings
