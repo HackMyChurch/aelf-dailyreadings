4096f34b424652c4608ec1f29670fb4028f9b3f0
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2017-05-28 12:03:36 +0200

    improve the error messages

--- app/src/main/java/co/epitre/aelf_lectures/DownloadXmlTask.java
+++ app/src/main/java/co/epitre/aelf_lectures/DownloadXmlTask.java
@@ -78,14 +78,12 @@ class DownloadXmlTask extends AsyncTask<WhatWhen, Void, List<LectureItem>> {
      * Error messages
      */
     private static final String networkErrorMessage = ""+
-            "<h3>Oups... Une erreur s'est glissée lors du chargement des lectures</h3>" +
+            "<h3>Une erreur s'est glissée lors du chargement des lectures</h3>" +
             "<p>Saviez-vous que cette application est développée entièrement bénévolement&nbsp;? Elle est construite en lien et avec le soutien de l'AELF, mais elle reste un projet indépendant, soutenue par <em>votre</em> prière&nbsp!</p>\n" +
             "<p>Si vous pensez qu'il s'agit d'une erreur, vous pouvez envoyer un mail à <a href=\"mailto:support@epitre.co?subject=Report:%20Network%20error%20loading%20##OFFICE##%20Office%20(version:%20##VERSION##)&body=##REPORT##\">support@epitre.co</a>.<p>";
     private static final String emptyOfficeErrorMessage = "" +
-            "<h3>Oups... Cet office ne contient pas de lectures</h3>" +
-            "<p>Cet office ne semble pas contenir de lecture. Si vous pensez qu'il s'agit d'un erreur, vous pouver essayer de \"Rafraîchir\" cet office.</p>" +
-            "<p>Saviez-vous que cette application est développée entièrement bénévolement&nbsp;? Elle est construite en lien et avec le soutien de l'AELF, mais elle reste un projet indépendant, soutenue par <em>votre</em> prière&nbsp!</p>\n" +
-            "<p>Si vous pensez qu'il s'agit d'une erreur, vous pouvez envoyer un mail à <a href=\"mailto:support@epitre.co?subject=Report:%20Empty%20%##OFFICE##20Office%20(version:%20##VERSION##)&body=##REPORT##\">support@epitre.co</a>.<p>";
+            "<h3>Il n'y a pas encore de lectures pour cet office</h3>" +
+            "<p>Le saviez-vous ? <a href=\"http://aelf.org/\">AELF</a> ajoute les nouveaux offices quotidiennement, jusqu'à un mois à l'avance&nbsp;! Celui-ci devrait bientôt arriver.</p>";
 
 
     public DownloadXmlTask(Context ctx, LectureLoadProgressListener lectureLoadProgressListener) {
@@ -194,7 +192,7 @@ class DownloadXmlTask extends AsyncTask<WhatWhen, Void, List<LectureItem>> {
         // Get version name
         String versionName = "";
         try {
-            versionName = "v" + ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionName;
+            versionName = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionName;
         } catch (PackageManager.NameNotFoundException e) {
             // Only drawback here is no version displayed in about. Minor anoyance
         }
@@ -241,7 +239,7 @@ class DownloadXmlTask extends AsyncTask<WhatWhen, Void, List<LectureItem>> {
         }
 
         // Build and return error
-        error.add(new LectureItem("error", "Oups...", message, null));
+        error.add(new LectureItem("error", "Erreur", message, null));
         return error;
     }
 
