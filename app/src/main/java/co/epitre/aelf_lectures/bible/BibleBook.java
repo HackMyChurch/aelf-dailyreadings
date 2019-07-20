b95feee7789f8da1a0f6e45b59379881c7449331
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2019-07-20 15:06:39 +0200

    bible v2: add book chapter list and text
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

---
+++ app/src/main/java/co/epitre/aelf_lectures/bible/BibleBook.java
@@ -0,0 +1,125 @@
+package co.epitre.aelf_lectures.bible;
+
+import android.content.res.AssetManager;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import co.epitre.aelf_lectures.LecturesApplication;
+
+public class BibleBook {
+    private final String BIBLE_DIR = "bible";
+
+    // Book status
+    private String mName;
+    private String mRef;
+    private ArrayList<BibleBookChapter> mChapters;
+
+    // Cache of books
+    private static HashMap<String, BibleBook> mBooks = new HashMap<>();
+
+    //
+    // Constructors
+    //
+
+    private BibleBook(@NonNull String ref, @NonNull String name) {
+        this.mName = name;
+        this.mRef = ref;
+    }
+
+    public static BibleBook getBook(@NonNull String ref, String name) {
+        // Try to load book from cache
+        BibleBook book = mBooks.get(ref);
+        if (book != null) {
+            return book;
+        }
+
+        // FIXME: We need the name to instanciate the book. This will become an issue when we access
+        // bible books by URL.
+        if (name == null) {
+            return null;
+        }
+
+        // Create the book instance and add it to the cache
+        book = new BibleBook(ref, name);
+        mBooks.put(ref, book);
+        return book;
+    }
+
+    //
+    // Accessors
+    //
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public String getRef() {
+        return this.mRef;
+    }
+
+    //
+    // API
+    //
+
+    public List<BibleBookChapter> getChapters() {
+        // Check if we have a cached version
+        if (this.mChapters != null) {
+            return this.mChapters;
+        }
+
+        // Allocate chapters list
+        this.mChapters = new ArrayList<>();
+
+        // Check if we have chapters
+        if (mRef == null) {
+            return this.mChapters;
+        }
+
+        AssetManager assets = LecturesApplication.getInstance().getAssets();
+
+        // Get all chapters
+        String[] files;
+        try {
+            files = assets.list(BIBLE_DIR+"/"+mRef);
+        } catch (IOException e) {
+            return this.mChapters;
+        }
+
+        for (String name : files) {
+            if (name.endsWith(".html")) {
+                BibleBookChapter chapter = new BibleBookChapter(mRef, name.split("\\.")[0]);
+                this.mChapters.add(chapter);
+            }
+        }
+
+        // If there is a single chapter, set the chapter title to the book name
+        if (this.mChapters.size() == 1) {
+            this.mChapters.get(0).setChapterName(mName);
+        }
+
+        // Sort the chapters in natural order
+        Collections.sort(this.mChapters, new BibleBookChapter.BibleBookChapterComparator());
+        return this.mChapters;
+    }
+
+    public BibleBookChapter getChapter(int position) {
+        return getChapters().get(position);
+    }
+
+    public int getChapterPosition(String chapterRef) {
+        int i = 0;
+        for (BibleBookChapter chapter: mChapters) {
+            if (chapter.getChapterRef().equals(chapterRef)) {
+                return i;
+            }
+            i++;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+}
--- app/src/main/java/co/epitre/aelf_lectures/bible/BibleBookEntry.java
+++ app/src/main/java/co/epitre/aelf_lectures/bible/BibleBookEntry.java
@@ -2,6 +2,9 @@ package co.epitre.aelf_lectures.bible;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
+
 enum BibleBookEntryType {
     SECTION(0),
     BOOK(1),
@@ -29,18 +32,79 @@ enum BibleBookEntryType {
 
 public class BibleBookEntry {
     private BibleBookEntryType mType;
-    private String mName;
+    private String mBookName;
+    private String mEntryName;
+    private String mBookRef;
+    private String mChapterRef;
+    private BibleBook mBook;
 
-    public BibleBookEntry(BibleBookEntryType type, @NonNull String name) {
-        this.mType = type;
-        this.mName = name;
+    // Section title entry constructor
+    public BibleBookEntry(@NonNull BibleBookEntryType type, @NonNull String bookName) {
+        this(type, bookName, null);
     }
+
+    // Book entry constructor
+    public BibleBookEntry(@NonNull BibleBookEntryType type, @NonNull String bookName, String bookRef) {
+        this(type, bookName, bookName, bookRef, null);
+    }
+
+    // Chapter entry constructor
+    public BibleBookEntry(@NonNull BibleBookEntryType type, @NonNull String bookName, String entryName, String bookRef, String chapterRef) {
+        this.mType = type;
+        this.mBookName = bookName;
+        this.mEntryName = entryName;
+        this.mBookRef = bookRef;
+        this.mChapterRef = chapterRef;
+    }
+
+    //
+    // Accessors
+    //
 
     public BibleBookEntryType getType() {
         return mType;
     }
 
-    public String getName() {
-        return this.mName;
+    public String getBookName() {
+        return this.mBookName;
+    }
+
+    public String getEntryName() {
+        return this.mEntryName;
+    }
+
+    public String getBookRef() {
+        return this.mBookRef;
+    }
+
+    public String getChapterRef() {
+        return this.mChapterRef;
+    }
+
+    //
+    // API
+    //
+
+    public BibleBook getBook() {
+        if (this.mBook == null) {
+            this.mBook = BibleBook.getBook(this.mBookRef, this.mBookName);
+        }
+        return this.mBook;
+    }
+
+    public List<BibleBookChapter> getChapters() {
+        BibleBook book = getBook();
+        if (book == null) {
+            return null;
+        }
+        return book.getChapters();
+    }
+
+    public BibleBookChapter getChapter(int position) {
+        BibleBook book = getBook();
+        if (book == null) {
+            return null;
+        }
+        return book.getChapter(position);
     }
 }
