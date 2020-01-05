3ef7ecc63d1ac139d210439e94a8dbd6111f7fd2
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2020-01-05 20:17:17 +0100

    bible-search: fix highligting of words before punctuation
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- app/src/main/assets/js/chapter.js
+++ app/src/main/assets/js/chapter.js
@@ -2,11 +2,12 @@
 if (highlight) {
     var instance = new Mark(document.querySelector("body"));
     instance.mark(highlight, {
-        "accuracy": "exactly",
+        "accuracy": {
+            "value": "exactly",
+            "limiters": ":;.,-–—‒_(){}[]!'\"+=".split(""),
+        },
         "ignoreJoiners": true,
         "acrossElements": true,
-        "ignoreJoiners": true,
-        "ignorePunctuation": ":;.,-–—‒_(){}[]!'\"+=".split(""),
         "wildcards": "enabled",
     });
 }
