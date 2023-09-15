HackMyChurch/aelf-dailyreadings

commit 83affc9a38d5b2c7a77108246e001869480cecfc

lint: add missing @NonNull annotations
Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

app/src/main/java/co/epitre/aelf_lectures/lectures/data/LecturesController.java
@@ -95,9 +95,6 @@ public final class LecturesController {
             return menu_id;
         }
 
-        public String toString(){
-            return name;
-        }
     }
 
 

app/src/main/java/co/epitre/aelf_lectures/LecturesActivity.java
@@ -228,7 +228,8 @@ public class LecturesActivity extends BaseActivity implements
         // Add some padding at the bottom of the drawer and content to account for the navigation bar
         drawerView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
             @Override
-            public WindowInsets onApplyWindowInsets(View view, WindowInsets windowInsets) {
+            @NonNull
+            public WindowInsets onApplyWindowInsets(@NonNull View view, @NonNull WindowInsets windowInsets) {
                 View navigationMenuView = findViewById(R.id.design_navigation_view);
                 navigationMenuView.setPaddingRelative(0, 0, 0, windowInsets.getSystemWindowInsetBottom());
                 return windowInsets.consumeSystemWindowInsets();
@@ -533,7 +534,7 @@ public class LecturesActivity extends BaseActivity implements
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
@@ -633,7 +634,7 @@ public class LecturesActivity extends BaseActivity implements
      */
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
         // Call parent (handles night mode)
         super.onConfigurationChanged(newConfig);
 

app/src/main/java/co/epitre/aelf_lectures/SectionFragmentBase.java
@@ -3,6 +3,8 @@ package co.epitre.aelf_lectures;
 import android.net.Uri;
 import android.os.Bundle;
 import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.ActionBar;
 import android.view.LayoutInflater;
@@ -26,7 +28,7 @@ public abstract class SectionFragmentBase extends Fragment {
     protected Menu mMenu;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Load global views
         activity = (LecturesActivity) getActivity();
         actionBar = activity.getSupportActionBar();
@@ -65,7 +67,7 @@ public abstract class SectionFragmentBase extends Fragment {
     //
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         mMenu = menu;
     }

app/src/main/java/co/epitre/aelf_lectures/base/BaseActivity.java
@@ -5,6 +5,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
@@ -59,7 +60,7 @@ public class BaseActivity extends AppCompatActivity implements
      */
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
         // Night mode change handling

app/src/main/java/co/epitre/aelf_lectures/bible/BibleBookFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.ListPopupWindow;
 import androidx.appcompat.widget.PopupMenu;
@@ -137,7 +138,7 @@ public class BibleBookFragment extends BibleFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Load global views

app/src/main/java/co/epitre/aelf_lectures/bible/BibleMenuBookListFragment.java
@@ -47,7 +47,7 @@ public class BibleMenuBookListFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Inflate the layout for this fragment

app/src/main/java/co/epitre/aelf_lectures/bible/BibleMenuFragment.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.viewpager.widget.ViewPager;
 
@@ -74,7 +75,7 @@ public class BibleMenuFragment extends BibleFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Load global views

app/src/main/java/co/epitre/aelf_lectures/bible/BibleSearchFragment.java
@@ -77,7 +77,7 @@ public class BibleSearchFragment extends BibleFragment implements
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Load global views
@@ -218,7 +218,7 @@ public class BibleSearchFragment extends BibleFragment implements
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
 
         // Expand the search box
@@ -242,12 +242,12 @@ public class BibleSearchFragment extends BibleFragment implements
         // Go back to the previous fragment when the search box is closed
         searchMenuItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
             @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
+            public boolean onMenuItemActionExpand(@NonNull MenuItem item) {
                 return false;
             }
 
             @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
+            public boolean onMenuItemActionCollapse(@NonNull MenuItem item) {
                 activity.onBackPressed();
                 return false;
             }

commit af861a521fed487c890217468d1e693426498ee6

sync: fix regression
Fix regression introduced in 83affc9a38d5b2c7a77108246e001869480cecfc.
The 'toString()' method was aggressively removed and caused the table
names to change.

Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

app/src/main/java/co/epitre/aelf_lectures/lectures/data/LecturesController.java
@@ -95,6 +95,10 @@ public final class LecturesController {
             return menu_id;
         }
 
+        public String toString(){
+            return name;
+        }
+
     }
 
 