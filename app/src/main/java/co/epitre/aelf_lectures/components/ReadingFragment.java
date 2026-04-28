b215267ef660977565402ba96cd7fdd3119a939d
Jean-Tiare Le Bigot <jt@yadutaf.fr>
2026-04-28 22:43:37 +0200

    accessibility: fix webview statemachine
    The tab content was invisible to screen reader on application resume.
    This is fixed by applying the setting on the root view.
    
    Once fixed, after resuming, the active tab is not the correct one. This
    is because they are created with accessibility enabled by default. This
    is not a problem when opening an office, but it is when resuming.
    
    Signed-off-by: Jean-Tiare Le Bigot <jt@yadutaf.fr>

--- app/src/main/java/co/epitre/aelf_lectures/components/ReadingFragment.java
+++ app/src/main/java/co/epitre/aelf_lectures/components/ReadingFragment.java
@@ -78,6 +78,9 @@ public abstract class ReadingFragment extends Fragment {
 
         // Load content
         loadText();
+
+        // Accessibility
+        setAccessibilityVisible(false);
     }
 
     @Override
@@ -102,18 +105,22 @@ public abstract class ReadingFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        setWebViewAccessibility(true);
+        setAccessibilityVisible(true);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        setWebViewAccessibility(false);
+        setAccessibilityVisible(false);
     }
 
-    private void setWebViewAccessibility(boolean visible) {
-        if (mWebView == null) return;
-        mWebView.setImportantForAccessibility(visible
+    private void setAccessibilityVisible(boolean visible) {
+        View rootView = getView();
+        if (rootView == null) {
+            return;
+        }
+
+        rootView.setImportantForAccessibility(visible
                 ? View.IMPORTANT_FOR_ACCESSIBILITY_YES
                 : View.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS
         );
